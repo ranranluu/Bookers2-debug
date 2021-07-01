9808737aa95a9b00709ad43e49f3350e7c53ae06
ranranluu <ranranluutennis@gmail.com>
2021-07-01 06:57:33 +0000

    [finish]Rspecも全てクリア

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
-  attachment :profile_image, destroy: false
+  has_many :books, dependent: :destroy
+  attachment :profile_image
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  
+  validates :introduction, length: {maximum: 50}
 end
